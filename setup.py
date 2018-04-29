be7f9db83e97ed9abba803f3adf2eda7f3918ba2
Clemens Lange <clemens.lange@cern.ch>
2018-04-29 15:57:13 +0200

    Add pylintrc, add pylint to tests, make setup.py pylint compatible, use https URLs, and add documentation URL

--- setup.py
+++ setup.py
@@ -1,41 +1,40 @@
-from setuptools import setup, find_packages
-from codecs import open
+"""pypi package setup."""
+import codecs
 from os import path
-import ROOT
+from setuptools import setup, find_packages
+import ROOT  # pylint: disable=W0611
 
-deps = [
-      'numpy',
-      'PyYAML'
-]
+DEPS = ['numpy', 'PyYAML']
 
-here = path.abspath(path.dirname(__file__))
+HERE = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with codecs.open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
-setup(name='hepdata_lib',
-      version='0.1.1',
-      description='Library for getting your data into HEPData',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='http://github.com/clelange/hepdata_lib',
-      author='Andreas Albert, Clemens Lange',
-      author_email='clemens.lange@cern.ch',
-      classifiers=[
+setup(
+    name='hepdata_lib',
+    version='0.1.1',
+    description='Library for getting your data into HEPData',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/clelange/hepdata_lib',
+    author='Andreas Albert, Clemens Lange',
+    author_email='clemens.lange@cern.ch',
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-      ],
-      keywords='HEPData physics OpenData',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      zip_safe=False,
-      install_requires=deps,
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      project_urls={
-        'Bug Reports': 'http://github.com/clelange/hepdata_lib/issues',
-        'Source': 'http://github.com/clelange/hepdata_lib',
-      },
-      )
+    ],
+    keywords='HEPData physics OpenData',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    zip_safe=False,
+    install_requires=DEPS,
+    setup_requires=['pytest-runner', 'pytest-pylint'],
+    tests_require=['pytest', 'pylint'],
+    project_urls={
+        'Documentation': 'https://hepdata-lib.readthedocs.io',
+        'Bug Reports': 'https://github.com/clelange/hepdata_lib/issues',
+        'Source': 'https://github.com/clelange/hepdata_lib',
+    }, )
