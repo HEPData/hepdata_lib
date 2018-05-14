ef9271368e218957d05f30879cb01285e204b5fc
aalbert <albert@physik.rwth-aachen.de>
2018-05-14 13:57:19 +0200

    fixup.

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -5,13 +5,18 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew cask uninstall --force oclint
     brew upgrade python
     brew install root
+
+
+    mkdir ./yaml
+    export PYTHONPATH="${PYTHONPATH}:$(pwd)/yaml"
+    easy_install --prefix="./yaml" git+https://github.com/yaml/pyyaml
+
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     curl -O https://root.cern.ch/download/root_v6.12.06.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
     tar xzvf root_v6.12.06.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
+
+    # Workaround for uggy PyYaml v 3.12 on pypi
+    # Can be removed once PyYaml 3.13 is released
+    pip install git+https://github.com/yaml/pyyaml
 fi
 
-# Workaround for uggy PyYaml v 3.12 on pypi
-# Can be removed once PyYaml 3.13 is released
-mkdir ./yaml
-export PYTHONPATH="${PYTHONPATH}:$(readlink -e ./yaml)"
-easy_install --prefix="./yaml" git+https://github.com/yaml/pyyaml
