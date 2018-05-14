0e62564fa6030330e438be2153f350f40fe51af3
aalbert <albert@physik.rwth-aachen.de>
2018-05-14 12:21:53 +0200

    fixup.

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -9,4 +9,7 @@ elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     curl -O https://root.cern.ch/download/root_v6.12.06.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
     tar xzvf root_v6.12.06.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
 fi
-pip install git:https://github.com/yaml/pyyaml
+
+# Workaround for uggy PyYaml v 3.12 on pypi
+# Can be removed once PyYaml 3.13 is released
+pip install git+https://github.com/yaml/pyyaml
