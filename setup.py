ae59eb615df4d20c5d27ce12ac30bf57d7c11a61
aalbert <albert@physik.rwth-aachen.de>
2018-05-15 13:47:15 +0200

    Test

--- setup.py
+++ setup.py
@@ -4,28 +4,46 @@ import codecs
 import os
 from os import path
 import subprocess
+import sys
+#~ from ctypes import cdll
 from setuptools import setup, find_packages
 
 def root_install():
     """Make sure ROOT is installed before import"""
     try:
-        import ROOT # pylint: disable=W0611
+        import ROOT #pylint: disable=unused-variable
         print("setup.py: ROOT already installed.")
     except ImportError:
         print("setup.py: Installing ROOT.")
         url = "https://root.cern.ch/download/root_v6.12.06.Linux-ubuntu14-x86_64-gcc4.8.tar.gz"
         subprocess.call(["curl", "-O", url])
-        subprocess.call("tar", "xzvf", path.basename(url))
+        subprocess.call(["tar", "xzf", path.basename(url)])
 
         cmd = ["bash", "-c", "cd root && source bin/thisroot.sh && env"]
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
+        #~ for lib in [x for x in os.listdir("./root/lib") if x.endswith(".so")]:
+            #~ try:
+                #~ cdll.LoadLibrary('./root/lib/'+lib)
+            #~ except:
+                #~ continue
+        try:
+            os.execv(sys.argv[0], sys.argv)
+        except OSError, exc:
+            print('Failed re-exec:', exc)
+            sys.exit(1)
+
         for line in proc.stdout:
             (key, _, value) = line.partition("=")
+            value = value.strip("\n")
             os.environ[key] = value
-
+            if(key in ["PYTHONPATH", "LIBPATH", "BIN_PATH", "ROOTSYS", "LD_LIBRARY_PATH"]):
+                for part in value.split(":"):
+                    sys.path.append(part)
+            print(key, value)
+        print(sys.path)
         proc.communicate()
-        import ROOT # pylint: disable=W0611
+        import ROOT #pylint: disable=unused-variable
 
 root_install()
 
