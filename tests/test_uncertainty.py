39b83f9b2331f4f12d0b9b56d145b38114d14bfe
AndreasAlbert <AndreasAlbert@users.noreply.github.com>
2018-05-31 11:29:19 +0200

    Improve 2D histogram reading and Uncertainty setter (#29)
    * 2D histograms: return more info to user.
    * Update docstring.
    * Add second setter function for uncertainty.
    * Add uncertainty test case.

--- tests/test_uncertainty.py
+++ tests/test_uncertainty.py
@@ -43,3 +43,29 @@ class TestUncertainty(TestCase):
                                 for x, y in zip(testvar.values, values)))
             self.assertTrue(all(test_utilities.float_compare(x, y)
                                 for x, y in zip(testunc.values, uncertainty)))
+
+    def test_set_values_from_intervals(self):
+        '''Test behavior of Uncertainy.test_set_values_from_intervals function'''
+
+        # Dummy central values and variatons relative to central value
+        npoints = 100
+        values = list(range(0, npoints, 1))
+        uncertainty = [(-random.uniform(0, 1), random.uniform(0, 1)) for _ in range(100)]
+
+        # Convert +- error to interval bounds
+        intervals = [(val + unc_minus, val + unc_plus)
+                     for val, (unc_minus, unc_plus) in zip(values, uncertainty)]
+
+        # Reference uses "normal" assignment
+        refunc = Uncertainty("reference_unc")
+        refunc.is_symmetric = False
+        refunc.values = uncertainty
+
+        # Test uses new function
+        testunc = Uncertainty("test_unc")
+        testunc.is_symmetric = False
+        testunc.set_values_from_intervals(intervals, nominal=values)
+
+        # Check that both agree
+        self.assertTrue(all([test_utilities.tuple_compare(tup1, tup2) \
+                        for tup1, tup2 in zip(testunc.values, refunc.values)]))
