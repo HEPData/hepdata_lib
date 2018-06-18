720d48f8def871c8cd550614db918b1ec5cf6305
AndreasAlbert <AndreasAlbert@users.noreply.github.com>
2018-06-18 10:39:34 +0200

    Submission: New function for adding additional resource files. (#38)
    * Submission: New function for adding additional resource files.
    
    * Add test for Submission.add_additional_resource
    
    * Port test for python 3

--- tests/test_submission.py
+++ tests/test_submission.py
@@ -1,6 +1,8 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 """Test Submission."""
+import os
+from builtins import bytes
 from unittest import TestCase
 from hepdata_lib import Submission, Table, Variable, Uncertainty
 
@@ -29,3 +31,35 @@ class TestSubmission(TestCase):
             test_submission.add_table(test_variable)
         with self.assertRaises(TypeError):
             test_submission.add_table(test_uncertainty)
+
+    def test_add_additional_resource_input_checks(self):
+        """Test the file checks in the add_additional_variable function."""
+
+        testpath = "./testfile.dat"
+        test_submission = Submission()
+
+        # Check with non-existant file
+        with self.assertRaises(RuntimeError):
+            test_submission.add_additional_resource("Some description", testpath, copy_file=True)
+
+        # Check with file that is too big
+        size = int(2e8) # bytes in 200 MB
+        with open(testpath, "wb") as f:
+            f.write(bytes("\0" * size,"utf-8"))
+        with self.assertRaises(RuntimeError):
+            test_submission.add_additional_resource("Some description", testpath, copy_file=True)
+
+        # Clean up
+        os.remove(testpath)
+
+        # Check with file that is not too big.
+        size = int(5e7) # bytes in 50 MB
+        with open(testpath, "wb") as f:
+            f.write(bytes("\0" * size,"utf-8"))
+        try:
+            test_submission.add_additional_resource("Some description", testpath, copy_file=True)
+        except RuntimeError:
+            self.fail("Submission.add_additional_resource raised an unexpected RuntimeError.")
+
+        # Clean up
+        os.remove(testpath)
