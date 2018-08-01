c30c2d92c98db487c9139ca1dae68c7d3c7ef2c9
Clemens Lange <clemens.lange@cern.ch>
2018-08-01 10:29:24 +0200

    Bump version: 0.2.2 → 0.2.3

--- docs/conf.py
+++ docs/conf.py
@@ -45,9 +45,9 @@ copyright = '2018, Andreas Albert, Clemens Lange'
 author = 'Andreas Albert, Clemens Lange'
 
 # The short X.Y version
-version = '0.2.2'
+version = '0.2.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.2.3'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with codecs.open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='hepdata_lib',
-    version='0.2.2',
+    version='0.2.3',
     description='Library for getting your data into HEPData',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
