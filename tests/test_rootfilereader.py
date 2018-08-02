5e520694a170a65c5006a71a505a782b9a072e1e
Andreas Albert <andreas.albert@cern.ch>
2018-08-02 14:36:20 +0200

    Refactoring.

--- tests/test_rootfilereader.py
+++ tests/test_rootfilereader.py
@@ -8,7 +8,7 @@ import ctypes
 import numpy as np
 import ROOT
 from test_utilities import float_compare, tuple_compare
-from hepdata_lib import RootFileReader
+from hepdata_lib.RootUtils import RootFileReader, get_hist_2d_points
 
 
 class TestRootFileReader(TestCase):
