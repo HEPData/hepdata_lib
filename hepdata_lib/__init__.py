1b1194f8542c2298f2a60c674fdaa69f59dad10a
AndreasAlbert <AndreasAlbert@users.noreply.github.com>
2018-12-13 16:05:03 +0100

    Update hepdata_lib/__init__.py
    Co-Authored-By: reimersa <arne.reimers@desy.de>

--- hepdata_lib/__init__.py
+++ hepdata_lib/__init__.py
@@ -137,7 +137,6 @@ class Variable(object):
         if self.units != '':
             tmp["header"] = {"name": self.name, "units": self.units}
         else:
-            tmp["header"] = {"name": self.name}
 
         if self.qualifiers:
             tmp["qualifiers"] = self.qualifiers
