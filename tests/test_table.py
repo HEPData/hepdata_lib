0b8824b0853d4a88cf1ab51c972b187b22ae6974
Andreas Albert <andreas.albert@cern.ch>
2018-12-13 17:03:25 +0100

    Add test for table.write_images
    Table tests: Add cleanup.
    
    Fixup table test.

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,8 +1,12 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 """Test Table."""
+import re
+import os
+import shutil
 from unittest import TestCase
-from hepdata_lib import Table, Variable, Uncertainty
+
+from hepdata_lib import Table, Variable, Uncertainty, helpers
 
 class TestTable(TestCase):
     """Test the Table class."""
@@ -34,9 +38,30 @@ class TestTable(TestCase):
         test_table = Table("Some Table")
         test_variable = Variable("Some Variable")
         test_table.add_variable(test_variable)
+        testdir = "test_output"
+        self.addCleanup(shutil.rmtree, testdir)
         try:
-            test_table.write_yaml("test_output")
+            test_table.write_yaml(testdir)
         except TypeError:
             self.fail("Table.write_yaml raised an unexpected TypeError.")
         with self.assertRaises(TypeError):
             test_table.write_yaml(None)
+        self.doCleanups()
+
+    def test_write_images(self):
+        """Test the write_images function."""
+
+        test_table = Table("Some Table")
+        search_path = re.sub("hepdata_lib.*","hepdata_lib", os.getcwd())
+
+        some_pdf = helpers.find_all_matching(search_path,"*.pdf")[0]
+        test_table.add_image(some_pdf)
+        testdir = "test_output"
+        self.addCleanup(shutil.rmtree, testdir)
+        try:
+            test_table.write_images(testdir)
+        except TypeError:
+            self.fail("Table.write_images raised an unexpected TypeError.")
+        with self.assertRaises(TypeError):
+            test_table.write_images(None)
+        self.doCleanups()
