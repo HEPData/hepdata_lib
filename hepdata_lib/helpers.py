8a2af3d839a8e0a74d87f5a130abfc97c196adcf
cippy <cippy91@gmail.com>
2020-06-12 17:20:20 +0200

    adding functions to manage rounding (#144)
    * adding functions to manage rounding
    * test for new function for rounding
    
    Co-authored-by: cippy <marco.cipriani@cern.ch>
    Co-authored-by: Clemens Lange <clemens.lange@cern.ch>

--- hepdata_lib/helpers.py
+++ hepdata_lib/helpers.py
@@ -8,6 +8,7 @@ import subprocess
 import fnmatch
 import math
 import numpy as np
+from six.moves import zip
 
 def execute_command(command):
     """
@@ -49,20 +50,167 @@ def find_all_matching(path, pattern):
     return result
 
 
-def relative_round(value, relative_digits):
-    """Rounds to a given relative precision"""
+def get_number_precision(value):
+    """
+    Get precision of an input value.
+    Exact integer powers of 10 are assigned same precision of smaller numbers
+    For example
+    get_number_precision(10.0) = 1
+    get_number_precision(10.001) = 2
+    get_number_precision(9.999) = 1
+    """
 
+    if isinstance(value, tuple):
+        return (get_number_precision(x) for x in value)
+
+    # if value is tuple, value == 0 might cause ValueError saying that
+    # 'The truth value of an array with more than one element is ambiguous'
+    # Thus, tuple is evaluated above
     if value == 0 or isinstance(value, str) or np.isnan(value) or np.isinf(value):
         return value
+
+    return math.ceil(math.log10(abs(value)))
+
+
+def relative_round(value, relative_digits):
+    """Rounds to a given relative precision"""
 
     if isinstance(value, tuple):
         return (relative_round(x, relative_digits) for x in value)
 
-    precision = math.ceil(math.log10(abs(value)))
+    if value == 0 or isinstance(value, str) or np.isnan(value) or np.isinf(value):
+        return value
 
-    absolute_digits = - precision + relative_digits
+    value_precision = get_number_precision(value)
+    absolute_digits = -value_precision + relative_digits
 
     return round(value, int(absolute_digits))
+
+
+def get_value_precision_wrt_reference(value, reference):
+    """
+    relative precision of first argument with respect to the second one
+    value and reference are both float and/or int
+    value can be float when reference is an int and viceversa
+
+    : param value: first value
+    : type  value: float, int
+
+    : param reference: reference value (usually the uncertainty on value)
+    : type  reference: float, int
+    """
+
+    this_function = "get_value_precision_wrt_reference()"
+    good_types = [int, float]
+    arguments = [value, reference]
+
+    # first check all arguments have appropriate type
+    for input_arg in arguments:
+        if not any(isinstance(input_arg, x) for x in good_types):
+            raise ValueError("Unsupported input type passed to " + this_function)
+
+    return get_number_precision(value) - get_number_precision(reference)
+
+
+def round_value_to_decimals(cont, key="y", decimals=3):
+    """
+    round all values in a dictionary to some decimals in one go
+    default round to 3 digits after period
+    possible use case: correlations where typical values are within -1,1
+
+    : param cont : dictionary as returned e.g. by RootFileReader::read_hist_1d()
+    : type  cont : dictionary
+
+    : param decimals: how many decimals for the rounding
+    : type  decimals: integer
+    """
+
+    decimals = int(decimals)
+
+    for i, val in enumerate(cont[key]):
+        if isinstance(val, tuple):
+            cont[key][i] = (round(val[0], decimals), round(val[1], decimals))
+        else:
+            cont[key][i] = round(val, decimals)
+
+
+def round_value_and_uncertainty_to_decimals(cont, val_key="y", unc_key="dy", decimals=3):
+    """
+    round values and uncertainty to some decimals
+    default round to 3 digits after period
+    possible use case: correlations where typical values are within -1,1
+
+    : param cont : dictionary as returned e.g. by RootFileReader::read_hist_1d()
+    : type  cont : dictionary
+
+    : param decimals: how many decimals for the rounding
+    : type  decimals: integer
+    """
+
+    decimals = int(decimals)
+
+    for i, (val, unc) in enumerate(zip(cont[val_key], cont[unc_key])):
+        cont[val_key][i] = round(val, decimals)
+        if isinstance(unc, tuple):
+            cont[unc_key][i] = (round(unc[0], decimals), round(unc[1], decimals))
+        else:
+            cont[unc_key][i] = round(unc, decimals)
+
+
+def round_value_and_uncertainty(cont, val_key="y", unc_key="dy", sig_digits_unc=2):
+    """
+    round values and uncertainty according to the precision of the uncertainty,
+    and also round uncertainty to a given number of significant digits
+    Typical usage:
+         reader = RootFileReader("rootfile.root")
+         data = reader.read_hist_1d("histogramName")
+         round_value_and_uncertainty(data,"y","dy",2)
+    will round data["y"] to match the precision of data["dy"] for each element, after
+    rounding each element of data["dy"] to 2 significant digits
+    e.g. 26.5345 +/- 1.3456 --> 26.5 +/- 1.3
+
+    : param cont : dictionary as returned e.g. by RootFileReader::read_hist_1d()
+    : type  cont : dictionary
+
+    : param sig_digits_unc: how many significant digits used to round the uncertainty
+    : type  sig_digits_unc: integer
+    """
+
+    sig_digits_unc = int(sig_digits_unc)
+
+    for i, (val, unc) in enumerate(zip(cont[val_key], cont[unc_key])):
+        if isinstance(unc, tuple):
+            # case for TGraphAsymmErrors with unc = (elow,ehigh), the central value is rounded
+            # using the significant digits of the largest of the two uncertainties,
+            # the smaller uncertainty would be rounded accordingly (at least 1 digit)
+            # usually lower and higher uncertainties will be of the same order of magnitude
+            # or at most different by 1 order (like +0.132  -0.083), in which case,
+            # if choosing 2 significant digits, the rounding should result in +0.13  -0.08
+            max_absunc = 0.0
+            index_min_unc = 0
+            # set default precision for both sides of uncertainty
+            sig_digits_unc_ntuple = [sig_digits_unc, sig_digits_unc]
+            if abs(unc[0]) < abs(unc[1]):
+                max_absunc = abs(unc[1])
+                index_min_unc = 0
+                relative_precision = get_value_precision_wrt_reference(unc[0], unc[1])
+            else:
+                max_absunc = abs(unc[0])
+                index_min_unc = 1
+                relative_precision = get_value_precision_wrt_reference(unc[1], unc[0])
+            # update precision on smaller uncertainty (at least 1 significant digit)
+            sig_digits_unc_ntuple[index_min_unc] = int(max(1, sig_digits_unc + relative_precision))
+            cont[unc_key][i] = (relative_round(unc[0], sig_digits_unc_ntuple[0]),
+                                relative_round(unc[1], sig_digits_unc_ntuple[1]))
+            val_precision = get_value_precision_wrt_reference(val, max_absunc)
+            sig_digits_val = int(sig_digits_unc + val_precision)
+            cont[val_key][i] = relative_round(val, sig_digits_val)
+        else:
+            # standard case for TH1 or TGraphErrors, uncertainty is a single value
+            cont[unc_key][i] = relative_round(unc, sig_digits_unc)
+            val_precision = get_value_precision_wrt_reference(val, unc)
+            sig_digits_val = int(sig_digits_unc + val_precision)
+            cont[val_key][i] = relative_round(val, sig_digits_val)
 
 
 def check_file_existence(path_to_file):
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,6 +6,9 @@ from unittest import TestCase
 import numpy as np
 
 from hepdata_lib.helpers import relative_round
+from hepdata_lib.helpers import get_number_precision
+from hepdata_lib.helpers import get_value_precision_wrt_reference
+from hepdata_lib.helpers import round_value_and_uncertainty
 
 
 class TestHelpers(TestCase):
@@ -30,3 +33,87 @@ class TestHelpers(TestCase):
         for original, digits, result in values:
             rounded = relative_round(original, digits)
             self.assertTrue(rounded == result)
+
+
+    def test_get_number_precision(self):
+        '''Test behavior of get_number_precision function'''
+
+        # Some values are mapped onto themselves
+        eigenvalues = [np.inf, 0, 'astring']
+        for eig in eigenvalues:
+            self.assertTrue(get_number_precision(eig) == eig)
+        self.assertTrue(np.isnan(get_number_precision(np.nan)))
+
+        # test case with single value
+        # test format is (original value, precision)
+        values = [
+            (12.5, 2),
+            (1.25, 1),
+            (0.125, 0),
+            (0.0125, -1)
+        ]
+        for value, prec in values:
+            precision = get_number_precision(value)
+            self.assertTrue(precision == prec)
+
+        # test case with ntuple (e.g. with two values)
+        # test format is (original value, precision)
+        # both original value and precision are ntuples (with two elements)
+        ntuples = {
+            (12.5, 1.25) : (2, 1),
+            (0.125, 0.0125) : (0, -1)
+        }
+        for key in ntuples:
+            prec1, prec2 = get_number_precision(key)
+            self.assertTrue((prec1, prec2) == ntuples[key])
+
+
+    def test_get_value_precision_wrt_reference(self):
+        '''Test behavior of get_value_precision_wrt_reference function'''
+
+        # test format is (value, reference, relative precision)
+        values = [
+            (12.5, 0.08, 3),
+            (1.25, 102.4, -2),
+            (0.0, 0.002, 2),
+            (10.0, 9, 0)
+        ]
+        for val, ref, prec in values:
+            precision = get_value_precision_wrt_reference(val, ref)
+            self.assertTrue(precision == prec)
+
+        # test wrong input type
+        with self.assertRaises(ValueError):
+            get_value_precision_wrt_reference(1.23, "bad")
+        with self.assertRaises(ValueError):
+            get_value_precision_wrt_reference(1.23, (1.2, 3.4))
+        with self.assertRaises(ValueError):
+            get_value_precision_wrt_reference("bad", (1.2, 3.4))
+
+
+    def test_round_value_and_uncertainty(self):
+        '''Test behavior of round_value_and_uncertainty function'''
+
+        # Test format is
+        # (container, key_for_values, key_for_uncertanties, significant_digits)
+        # uncertainty has a single value
+        cont = {"val": [1.23456, 1234.56, 0.0012345, 0.123],
+                "unc": [0.00123, 1.23, 0.012, 0.12],
+                "val_round": [1.2346, 1234.6, 0.001, 0.12],
+                "unc_round": [0.0012, 1.2, 0.012, 0.12]}
+        # round to two significant digits
+        round_value_and_uncertainty(cont, "val", "unc", 2)
+        self.assertTrue(cont["val"] == cont["val_round"])
+        self.assertTrue(cont["unc"] == cont["unc_round"])
+
+        # Test format is
+        # (container, key_for_values, key_for_uncertanties, significant_digits)
+        # uncertainty has two value, as it would be the case with TGraphAsymmErrors
+        cont_asymm_err = {"val": [1.23456, 0.123],
+                          "unc": [(0.00123, 0.0123), (0.012, 0.12)],
+                          "val_round": [1.235, 0.12],
+                          "unc_round": [(0.001, 0.012), (0.01, 0.12)]}
+        # round to two significant digits
+        round_value_and_uncertainty(cont_asymm_err, "val", "unc", 2)
+        self.assertTrue(cont_asymm_err["val"] == cont_asymm_err["val_round"])
+        self.assertTrue(cont_asymm_err["unc"] == cont_asymm_err["unc_round"])
