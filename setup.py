6647715b3b8c1fe0795e7dbb13c8cebbeb384cb5
Clemens Lange <clemens.lange@cern.ch>
2021-10-28 18:37:42 +0200

    Make pytest requirements test and not setup requirements

--- setup.py
+++ setup.py
@@ -37,10 +37,11 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     zip_safe=False,
     install_requires=DEPS,
-    setup_requires=['pytest-runner', 'pytest-cov'],
+    setup_requires=[],
     tests_require=['pytest', 'papermill', 'six',
                    'pylint==1.9.5; python_version<"3"',
                    'pylint==2.9.6; python_version>="3"',
+                   'pytest-runner', 'pytest-cov',
                   ],
     project_urls={
         'Documentation': 'https://hepdata-lib.readthedocs.io',
