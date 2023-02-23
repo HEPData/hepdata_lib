53445de01f88f479d4ba4c85c07fb3dce493c78c
Matthew Feickert <matthew.feickert@cern.ch>
2023-02-23 01:09:14 -0700

    build: Add requires-python metadata
    * Add requires-python metadata through the addition of setuptools's
      python_requires in setup.py.
       - c.f. https://peps.python.org/pep-0621/#requires-python
      The lower bound versions are selected to support the versions of
      CPython currently supported: 2.7 and 3.6+
    
      Currently these lower bound versions of CPython (2.7, 3.6) have
      all reached EOL:
    
     ┌───────┬────────────┬─────────┬────────────────┬────────────┐
     │ cycle │  release   │ latest  │ latest release │    eol     │
     ├───────┼────────────┼─────────┼────────────────┼────────────┤
     │  3.6  │ 2016-12-22 │  3.6.15 │   2021-09-03   │ 2021-12-23 │
     │  3.5  │ 2015-09-12 │  3.5.10 │   2020-09-05   │ 2020-09-13 │
     │  3.4  │ 2014-03-15 │  3.4.10 │   2019-03-18   │ 2019-03-18 │
     │  3.3  │ 2012-09-29 │  3.3.7  │   2017-09-19   │ 2017-09-29 │
     │  2.7  │ 2010-07-03 │  2.7.18 │   2020-04-19   │ 2020-01-01 │
     └───────┴────────────┴─────────┴────────────────┴────────────┘
    
      The addition of requires-python is to provide guards to keep older
      CPython versions from installing releases that could contain
      unrunnable code.

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     keywords='HEPData physics OpenData',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     zip_safe=False,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     install_requires=DEPS,
     setup_requires=['pytest-runner', 'pytest-cov'],
     tests_require=['pytest', 'papermill', 'six',
