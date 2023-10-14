1aea7b2577bf41e2be9ba1b5def28c1682eb53bb
Graeme Watt <Graeme.Watt@durham.ac.uk>
2023-10-14 02:01:16 +0100

    ci: remove Python v3.11 from test matrix
    * Python v3.11 incompatible with pylint v2.9.6 (installs wrapt v1.12.1).
    * Add upper pin python_requires='>=3.6,<3.11' in setup.py.
    * Use Python v3.10 in "Read the Docs" configuration file.

--- setup.py
+++ setup.py
@@ -33,12 +33,11 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
     ],
     keywords='HEPData physics OpenData',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     zip_safe=False,
-    python_requires='>=3.6',
+    python_requires='>=3.6,<3.11',
     install_requires=DEPS,
     extras_require={
         'test': [
