7ddb4556bafc255b5726daea5efb8cc93f7c5074
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-10-30 13:33:36 +0100

    Added unit test for hist_utils functions

--- tests/test_histread.py
+++ tests/test_histread.py
@@ -3,6 +3,7 @@
 from unittest import TestCase
 import numpy as np
 import hist
+import hist.intervals
 from hepdata_lib.hist_utils import *
 
 
@@ -40,6 +41,7 @@ class TestHistUtils(TestCase):
     )
 
     def test_default_read(self):
+        # Default read with no projection
         try:
             readout = read_hist(TestHistUtils.base_hist)
         except:
@@ -50,5 +52,101 @@ class TestHistUtils(TestCase):
         self.assertTrue(len(readout["dataset"]) == len(readout["eta"]))
         self.assertTrue(len(readout["dataset"]) == len(readout["pt"]))
 
+    def test_projection_read(self):
+        # Default read with simple projection
+        try:
+            r1 = read_hist(TestHistUtils.base_hist[{"dataset": "data", "flavor": sum}])
+            r2 = read_hist(TestHistUtils.base_hist[{"dataset": "QCD", "flavor": 0j}])
+        except:
+            self.fail("Histogram reading raised an unexpected exception.")
+        # Checking dimension compatibility
+        self.assertTrue(len(r1["eta"]) == len(r1["pt"]))
+        self.assertTrue(len(r1["eta"]) == len(r2["pt"]))
+        self.assertTrue(np.all(r1["eta"] == r2["eta"]))
+        self.assertTrue(np.all(r1["pt"] == r2["pt"]))
+
         # Clean up
+        self.doCleanups()
+
+    def test_uncertainty_generation(self):
+        h = TestHistUtils.base_hist[{"dataset": "data"}]
+        q_h = TestHistUtils.base_hist[{"dataset": "QCD"}]
+        t_h = TestHistUtils.base_hist[{"dataset": "ttbar"}]
+
+        h_arr = h.view(flow=True)["value"].flatten()
+        unc_arr = np.ones_like(h_arr) * np.random.random(size=h_arr.shape[0])
+        try:
+            r = hist_as_variable(
+                "testing",
+                h,
+                flow=True,
+                uncertainty={
+                    "symmetric stat": "poisson_sym",
+                    "asymmetric stat": "poisson_asym",
+                    "symmetric float": 1.5,
+                    "asymmetric float": (1.5, 2.2),
+                    "symmetric array": unc_arr,
+                    "asymmetric array": (-0.8 * unc_arr, unc_arr),
+                    "symmetric histogram": q_h,
+                    "asymmetric histogram": (q_h, t_h),
+                },
+            )
+        except:
+            self.fail("Unexpected exception of automatic uncertainty generation.")
+
+        def check_val(a, b):
+            return self.assertTrue(np.all(np.isclose(a, b) | np.isnan(a) | np.isnan(b)))
+
+        h_arr = h.view(flow=True)["value"].flatten()
+        check_val(r.values, h_arr)
+        # Symmetric Poisson
+        check_val(r.uncertainties[0].values, np.sqrt(h_arr))
+
+        # Asymmetric Poisson
+        l, u = hist.intervals.poisson_interval(h_arr)
+        l, u = l - h_arr, u - h_arr
+        check_val(r.uncertainties[1].values, list(zip(l, u)))
+
+        # Flat uncertainties
+        check_val(r.uncertainties[2].values, 1.5)
+        check_val(r.uncertainties[3].values, (1.5, 2.2))
+
+        # Array defined uncertainties
+        check_val(r.uncertainties[4].values, unc_arr)
+        check_val(r.uncertainties[5].values, list(zip(-0.8 * unc_arr, unc_arr)))
+
+        # Histogram defined uncertainties
+        q_arr = q_h.view(flow=True)["value"].flatten()
+        t_arr = t_h.view(flow=True)["value"].flatten()
+        check_val(r.uncertainties[6].values, q_arr)
+        check_val(r.uncertainties[7].values, list(zip(q_arr, t_arr)))
+
+        self.doCleanups()
+
+    def test_table_generation(self):
+        _rename_ = {
+            "dataset": "Data set",
+            "flavor": "Jet flavor",
+            "eta": "Jet $\eta$",
+            "pt": "Jet $p_{T}$",
+        }
+        _units_ = {"pt": "GeV"}
+        try:
+            table = create_hist_base_table(
+                "my_table",
+                TestHistUtils.base_hist,
+                axes_rename=_rename_,
+                axes_units=_units_,
+            )
+        except:
+            self.fail("Unexpected exception of table generation.")
+
+        readout = read_hist(TestHistUtils.base_hist)
+
+        for idx, (name, new_name) in enumerate(_rename_.items()):
+            # Checking rename
+            self.assertTrue(table.variables[idx].name == new_name)
+            self.assertTrue(table.variables[idx].units == _units_.get(name, ""))
+            self.assertTrue(len(table.variables[idx].values) == len(readout[name]))
+
         self.doCleanups()
